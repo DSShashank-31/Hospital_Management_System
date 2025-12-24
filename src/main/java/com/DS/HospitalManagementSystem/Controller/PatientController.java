e5e54b559621916f296abcef9dc2ca16bf25d647
DSShashank-31 <dsshashank01@gmail.com>
2025-12-24 21:31:59 +0530

    commit

--- src/main/java/com/DS/HospitalManagementSystem/Controller/PatientController.java
+++ src/main/java/com/DS/HospitalManagementSystem/Controller/PatientController.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.DS.HospitalManagementSystem.Dto.PatientDto;
 import com.DS.HospitalManagementSystem.services.PatientServices;
 
-@Controller
+@RestController
+@RequestMapping("/patient")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PatientController {
 	
 	@Autowired
